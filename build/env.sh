03752ba5b12ec21a610ef31d5f29eea59caa8158
Jacky Chan <chankongching@gmail.com>
2018-01-30 17:54:16 +0800

    Updating go-eubchain comment

--- build/env.sh
+++ build/env.sh
@@ -11,10 +11,10 @@ fi
 workspace="$PWD/build/_workspace"
 root="$PWD"
 ethdir="$workspace/src/github.com/ethereum"
-if [ ! -L "$ethdir/go-ethereum" ]; then
+if [ ! -L "$ethdir/go-eubchain" ]; then
     mkdir -p "$ethdir"
     cd "$ethdir"
-    ln -s ../../../../../. go-ethereum
+    ln -s ../../../../../. go-eubchain
     cd "$root"
 fi
 
@@ -23,8 +23,8 @@ GOPATH="$workspace"
 export GOPATH
 
 # Run the command inside the workspace.
-cd "$ethdir/go-ethereum"
-PWD="$ethdir/go-ethereum"
+cd "$ethdir/go-eubchain"
+PWD="$ethdir/go-eubchain"
 
 # Launch the arguments with the configured environment.
 exec "$@"
